7a0438fc2cd865e4046f23a48726ad720713c402
vahyno <vahyno@gmail.com>
2018-07-23 13:12:50 -0700

    api's tested and working

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var mongoose = require('mongoose');
 var config = require('./config');
 var setupController = require('./controllers/setupController');
+var apiController = require('./controllers/apiController');
 
 var port = process.env.PORT || 3000;
 
@@ -11,7 +12,9 @@ app.use('/assets', express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 mongoose.connect(config.getDbConectionString(), { useNewUrlParser: true });
+
 setupController(app);
+apiController(app);
 
 app.listen(port, function() {
     console.log(`Listening at localhost ${ port }`);
